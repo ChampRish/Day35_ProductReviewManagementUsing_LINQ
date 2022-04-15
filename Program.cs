0d0c31ab6199d86d180b952865dbb60f49f4ade5
RISHABH <gguptarishabh82@gmail.com>
2022-04-15 14:18:53 +0530

    Skip top 5 records and display - implemented

--- Program.cs
+++ Program.cs
@@ -47,6 +47,7 @@ namespace Day35_ProductReviewManagement
             pm.RetriveRecords(ProductList);
             pm.CountOfUser(ProductList);
             pm.ShowProductIDAndReview(ProductList);
+            pm.SkipTop5Records(ProductList);
             Console.ReadLine();
 
         }
